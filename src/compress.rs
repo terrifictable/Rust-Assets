ae272cbac443ff5a768bf88e4ce31e3c06f75f80
TerrificTable <omegagaming189@gmail.com>
2023-02-13 00:24:15 +0100

    'proper' file reading now (also supports non utf8 encoded files now)

--- src/compress.rs
+++ src/compress.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, io::{BufRead, BufReader, Read}};
 use base64::{Engine as _, engine::general_purpose};
 
 
@@ -11,9 +11,13 @@ pub fn generate_dir_assets(dir: &str) -> String {
     let mut result: String = Default::default();
     for _path in paths {
         let path = _path.unwrap();
-        let contents = fs::read_to_string(path.path()).unwrap();
         
-        let whyyyyy = general_purpose::STANDARD.encode(contents.as_bytes());
+        let mut file = fs::File::open(path.path().to_str().unwrap()).expect("Failed to open file");
+        let mut reader = BufReader::new(&file);
+        let mut contents = Vec::new();
+        reader.read_to_end(&mut contents);
+        
+        let whyyyyy = general_purpose::STANDARD.encode(contents);
         val = whyyyyy.as_str();
         val_string = " \"".to_string() + val + &"\"".to_string();
 
