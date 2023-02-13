60a9e489ea0c3342cd19f7113ba9ee6112600c8b
TerrificTable <omegagaming189@gmail.com>
2023-02-13 20:20:48 +0100

    some simple utils for the cli (only one of them is being used, but still)

---
+++ src/cli_utils.rs
@@ -0,0 +1,42 @@
+use std::process::{Command, Stdio};
+
+
+lazy_static::lazy_static! {
+    static ref BUILD_BRANCH_NAME: String = run_command("git", vec!["rev-parse", "--abbrev-ref", "HEAD"]).replace("\n", "");
+    static ref BUILD_COMMIT: String = run_command("git",  vec!["rev-parse", "--short", "HEAD"]).replace("\n", "");
+
+    static ref VERSION_STRING: String = get_version_string();   
+}
+
+
+// pub(crate) fn write_help() {
+//     println!("rust-assets {}
+//     -h | --help     : show help
+//     -v | --version  : show version
+//     -o | --output   : output filename
+//     -d | --dir      : dir to include (recursiveley)\n", VERSION_STRING.as_str());
+// }
+
+pub(crate) fn write_version() {
+    println!("rust-assets v.{}", VERSION_STRING.as_str())
+}
+
+
+
+
+fn get_version_string() -> String {
+    format!("0.1.0 ({}/{})", BUILD_BRANCH_NAME.as_str(), BUILD_COMMIT.as_str())
+}
+
+fn run_command(command: &str, args: Vec<&str>) -> String {
+    let output = Command::new(command)
+            .args(args)
+            .stdout(Stdio::piped())
+            .output()
+            .unwrap_or_else(|e| {
+                panic!("Failed to execute process: {}", e)
+            });
+    
+    String::from_utf8(output.stdout).unwrap()
+}
+
