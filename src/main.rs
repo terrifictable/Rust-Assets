da290383bbb9d7d8c448961bedb965f4e4808329
TerrificTable <omegagaming189@gmail.com>
2023-02-13 20:28:33 +0100

    the main file... it does the 'cli' stuff, also if i figure out how to add a custom help menu to clap i will do so

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
+use std::process::exit;
+use clap::Parser;
+
+mod cli_utils;
 mod compress;
 
+#[derive(Parser,Debug)]
+struct ArgsParser {
+    #[clap(short, long, action)]
+    version: bool,
+    
+    #[clap(short, long, default_value_t = String::from("assets.rs"))]
+    output: String,
+    
+    #[clap(short, long, default_value_t = String::from(""))]
+    dir: String,
+}
 
 fn main() {
+    let args = ArgsParser::parse();
 
-    let dirs = vec!["test"];
-    compress::create_assets_file(dirs, "test");
+    if args.version {
+        cli_utils::write_version();
+        exit(0);
+    }
 
+    if !args.output.is_empty() && !args.dir.is_empty() {
+        compress::create_assets_file(vec![args.dir.as_str()], args.output.as_str());
+    }
 }
